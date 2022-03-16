46d477af5fe3d0a02b18253e515956d7e908683f
rahmaauliafani <rahmaauliafani@gmail.com>
2022-03-16 10:16:43 +0700

    Commit pertama

---
+++ app/src/main/java/com/example/mymusic/Musik.java
@@ -0,0 +1,23 @@
+package com.example.mymusic;
+
+import android.net.Uri;
+
+import java.io.File;
+
+public class Musik extends File {
+
+    private final Uri uri;
+    private final  String title;
+
+    public Musik(Uri uri, String title) {
+        super(uri.toString());
+        this.uri = uri;
+        this.title = title;
+    }
+    public  Uri getUri() {
+        return uri;
+    }
+    public String getTitle(){
+        return  title;
+    }
+}
